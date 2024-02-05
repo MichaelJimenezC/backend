e1d851ee442cb484c0c80d8f080d756677fc6000
MichaelJimenezC <micjejim@espol.edu.ec>
2024-02-04 21:49:45 -0500

    puerto

--- db.js
+++ db.js
@@ -6,6 +6,7 @@ const datosModel = require('./models/datos')
 
     const sequelize = new Sequelize('railway', 'root', 'dG-hCGfgb-DBA6FC3B-gg53F2fBfHA4B', {
        host: 'viaduct.proxy.rlwy.net',
+       port: '57517',
       dialect: 'mysql' ,
       define: { 
           timestamps: false
